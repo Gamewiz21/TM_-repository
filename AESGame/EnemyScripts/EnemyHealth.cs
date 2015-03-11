94815b647386f6be8c02d6756ea6b7cfd9037166
Gamewiz21 <terrance.franklin@loop.colum.edu>
2015-03-10 20:03:41 -0500

    Update

---
+++ AESGame/EnemyScripts/EnemyHealth.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : EnemyLifeManager {
+
+	public SwordHitBox hitBox;
+    // Use this for initialization
+
+	void Awake() 
+	{	// hitbox equals the components from the SwordHitBox script
+		hitBox = GameObject.Find("Hitbox0").GetComponent<SwordHitBox>(); 
+
+	}
+    protected override void Start()
+    {
+        base.Start();
+
+	
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// if the player presses 0 and InRange is set as true
+		if (Input.GetKeyDown (KeyCode.O) && hitBox.InRange == true) 
+		{ // enemy loses 30 health
+			enemyhealth -= 30;
+			//Debug.Log("hit");
+		}
+		// if enemyhealth equal 0
+        if (enemyhealth <= 0)
+        { 	// destroy gameObject
+            Destroy (this.gameObject);
+        }
+
+    }
+
+   
+
+    void OnTriggerEnter2D(Collider2D other)
+    {	//if the weapon itself touches the collider
+        if (other.gameObject.tag == "Weapon")
+        {	//enemy loses 30 health
+            enemyhealth -= 30;
+        }
+
+       
+    }
+}
---
+++ AESGame/MyScripts/SlashAttack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlashAttack : MonoBehaviour {
+
+	Animator Anim;
+
+	// Use this for initialization
+	void Awake () {
+
+		Anim = GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.O))
+            Anim.Play("Swords");
+        
+			
+	
+	}
+}
