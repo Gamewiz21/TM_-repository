dc90b3c5ddd5e070d110b5bdf7bbe031a80f1dfd
Gamewiz21 <terrance.franklin@loop.colum.edu>
2015-04-07 19:40:08 -0500

    Update and Shaders
    I fixed some bugs and trying to work with some remaining mechanics. I
    also have the cloud movement shader working

---
+++ AESGame/Assets/CountingCrystal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountingCrystal : MonoBehaviour {
+
+	public int crystalCount;
+	public GUIStyle HUD;
+	// Use this for initialization
+	void Start () {
+		crystalCount = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		GUI.Label (new Rect (10, 10, 200, 100), "Crystals: " + crystalCount,HUD);
+	
+	}
+}
---
+++ AESGame/Assets/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : HealthManager {
+
+    public GUIStyle HUD;
+	// Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+
+     }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 30, 300, 150), "Health: " + health,HUD);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Spike")
+        {
+            health -= 10;
+        }
+    }
+}
